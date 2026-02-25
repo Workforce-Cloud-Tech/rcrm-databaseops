faa43141e1930f8b2c31a4d57f77c30109c8ffbe
jayp19 <81046430+jayp19@users.noreply.github.com>
2026-02-25 19:38:59 +0530

    added not null

--- migrations-semver/1.1.0_testtable.sql
+++ migrations-semver/1.1.0_testtable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS testtable (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    phonenumber VARCHAR(20)
+    name VARCHAR(255) not null,
+    email VARCHAR(255) not null,
+    phonenumber VARCHAR(20) not null
 );
